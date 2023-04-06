50dc5edb5b5a52fd315668f92162ad4d2bc27f84
Victor Paiva <129222373+victor-paivas@users.noreply.github.com>
2023-04-06 16:04:47 -0300

    AULA 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+var c= 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c recebe + 1
+}while ( c <= 6)
+
+
+/*  A ESTRUTURA MAIS ENCONTRADA NO MEIO DOS PROGAMAS
+var c= 1
+while (c <= 1){
+    console.log(`Passo ${c}`)
+    c++ // c recebe + 1
+}
+*/
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
+
