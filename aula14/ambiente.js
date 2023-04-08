dc0b785f52b21d0a2228edbf82afc0008ec97375
Victor Paiva <129222373+victor-paivas@users.noreply.github.com>
2023-04-08 12:10:51 -0300

    Create ambiente.js

---
+++ aula14/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1
+
+//Estrutura de repetição com teste lógico no inicio
+while (c <=5) {
+    console.log(c)
+    c++
+}
+console.log('Estrutura de repetição com teste lógico no inicio')
+//-----------------------------------------------------
+
+//Estrutura de repetição com teste lógico no final
+do {
+    console.log(c)
+    c++
+} while (c <=8) 
+console.log('Estrutura de repetição com teste lógico no final')
+//---------------------------------------------------
+
+//Estrutura de repetição com variável de controle
+for (var c=1; c<=10; c++) {
+    console.log(c)
+}
+console.log('Estrutura de repetição com variável de controle')
