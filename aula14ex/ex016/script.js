a1bf16582abf2e679be9e525992a4c61d19d0ea6
Victor Paiva <129222373+victor-paivas@users.noreply.github.com>
2023-04-08 16:09:14 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -3,22 +3,30 @@ function contar() {
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
     let res = document.getElementById('res')
-     
-    if (ini.value.lenght == 0 || fim.value.lenght == 0 || passo.value.length ==0) {
-        window.alert('[ERRO] Faltam dados!')
-    }else {
-        res.innerHTML = `Contando:`
-    }
 
-
-
-
-
-
-
-
-
+    if (ini.value.lenght == 0 || fim.value.lenght == 0 || passo.value.length == 0) {
+        window.alert('[ERRO]Faltam dados!')
+        res.innerHTML ='Impossível contar!'
+    } else {
+        res.innerHTML = `Contando: `
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (i < f) {
+            //Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }           
+        } else {
+            //Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449} `
+            }        
+        }
+        res.innerHTML += ` \u{1F3C1}`
+    }    
 }
+
 
 
 
