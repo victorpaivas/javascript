f3b0dcef16c123b30f361a18f1879ab7db68cf2a
Victor Paiva <129222373+victor-paivas@users.noreply.github.com>
2023-04-08 15:04:09 -0300

    Ex17

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,21 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+     
+    if (ini.value.lenght == 0 || fim.value.lenght == 0 || passo.value.length ==0) {
+        window.alert('[ERRO] Faltam dados!')
+    }else {
+        res.innerHTML = `Contando:`
+    }
+
+
+
+
+
+
+
+
+    
+}
